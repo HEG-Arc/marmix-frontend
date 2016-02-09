2e25680bc1958c9e56ea0431a8f6cd9a54fdef0b
Boris Fritscher <boris@fritscher.ch>
2016-02-09 10:03:27 +0100

    fix filter of undefined

--- app/scripts/filters/startfrom.js
+++ app/scripts/filters/startfrom.js
@@ -12,6 +12,9 @@ angular.module('marmixApp')
   .filter('startFrom', function () {
     return function(input, start) {
         start = +start; //parse to int
-        return input.slice(start);
+        if(input && input.slice){
+            return input.slice(start);
+        }
+        return  input;
     };
   });
